1c27a290fb312a1569ec1fb4e5476b83ff8ef146
ykvelandia <velandia416@gmail.com>
2024-02-20 20:49:39 -0500

    first commit

---
+++ src/test/java/com/classMongo/bookings/BookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classMongo.bookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
